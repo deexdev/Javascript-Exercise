a1cc05cd1d18c6379c0ad474843f0ac4dace0f68
deexdev <dwiprasetiyo009@gmail.com>
2024-02-24 17:21:33 +0700

    hitung luas segitiga dan luas permukaan tabung

---
+++ problem4.js
@@ -0,0 +1,5 @@
+let alas = 20;
+let tinggi = 25;
+
+let luasSegitiga = 0.5 * alas * tinggi;
+console.log(luasSegitiga);
---
+++ problem5.js
@@ -0,0 +1,10 @@
+//luas permukaan tabung
+
+let T = 20;
+let r = 4;
+
+let pi = 3.14;
+let first = r + T;
+
+let luasPermukaanTabung = 2 * pi * r * first
+console.log(luasPermukaanTabung);
